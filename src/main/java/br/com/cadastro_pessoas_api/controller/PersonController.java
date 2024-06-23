a58a7cca93bbaa6a0d0aa970823883cd158271eb
Npx <cheyprivado@gmail.com>
2024-06-23 20:06:55 -0300

    Atualizando endpoints

--- src/main/java/br/com/cadastro_pessoas_api/controller/PersonController.java
+++ src/main/java/br/com/cadastro_pessoas_api/controller/PersonController.java
@@ -1,12 +1,23 @@
 package br.com.cadastro_pessoas_api.controller;
 
 import br.com.cadastro_pessoas_api.dto.PersonRequestDTO;
+import br.com.cadastro_pessoas_api.model.NaturalPerson;
+import br.com.cadastro_pessoas_api.model.Person;
+import br.com.cadastro_pessoas_api.service.LegalPersonService;
+import br.com.cadastro_pessoas_api.service.NaturalPersonService;
 import br.com.cadastro_pessoas_api.service.PersonService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -14,9 +25,16 @@ public class PersonController {
   @Autowired
   private PersonService personService;
 
+  @Autowired
+  private NaturalPersonService naturalPersonService;
+
   @PostMapping
-  public ResponseEntity<Object> addPerson(@RequestBody @Valid PersonRequestDTO person) {
-    return ResponseEntity.ok(personService.save(person));
+  public ResponseEntity<Person> addPerson(@RequestBody @Valid PersonRequestDTO person, UriComponentsBuilder uriComponentsBuilder) {
+    Person savedPerson = personService.save(person);
+
+    var uri = uriComponentsBuilder.path("/pessoas/{id}").buildAndExpand(savedPerson.getId()).toUri();
+
+    return ResponseEntity.created(uri).body(savedPerson);
   }
 
   @DeleteMapping("/{id}")
@@ -26,7 +44,42 @@ public class PersonController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Object> getPersonById(@PathVariable Long id) {
+  public ResponseEntity<Person> getPersonById(@PathVariable Long id) {
     return ResponseEntity.ok(personService.getById(id));
+  }
+
+  @GetMapping("/nome")
+  public ResponseEntity<Person> getPersonByName(@RequestBody Map<String, String> params) {
+    return ResponseEntity.ok(personService.getByName(params.get("name")));
+  }
+
+  @GetMapping("/documento")
+  public ResponseEntity<Person> getPersonByDocument(@RequestBody Map<String, String> params) {
+    return ResponseEntity.ok(personService.getByDocument(params.get("type"), params.get("document")));
+  }
+
+  @GetMapping
+  public ResponseEntity<Page<Person>> getAllPersons(@RequestParam(name = "type", required = false) String type, @PageableDefault(size = 5, page = 0) Pageable pageable) {
+    return ResponseEntity.ok(personService.getAll(type, pageable));
+  }
+
+  @GetMapping("/nome/asc")
+  public ResponseEntity<Page<Person>> getAllPersonsSortedFromAToZ(@PageableDefault(size = 5, page = 0, sort = "name") Pageable pageable) {
+    return ResponseEntity.ok(personService.getAllSorted(pageable));
+  }
+
+  @GetMapping("/nome/desc")
+  public ResponseEntity<Page<Person>> getAllPersonsSortedFromZToA(@PageableDefault(size = 5, page = 0, sort = "nome", direction = Sort.Direction.DESC) Pageable pageable) {
+    return ResponseEntity.ok(personService.getAllSorted(pageable));
+  }
+
+  @GetMapping("/pf/ddn/asc")
+  public ResponseEntity<Page<NaturalPerson>> getAllPersonsSortedByDateOfBirthAsc(@PageableDefault(size = 5, page = 0, sort = "dateOfBirth") Pageable pageable) {
+    return ResponseEntity.ok(naturalPersonService.getAllSortedByDateOfBirth(pageable));
+  }
+
+  @GetMapping("/pf/ddn/desc")
+  public ResponseEntity<Page<NaturalPerson>> getAllPersonsSortedByDateOfBirthDesc(@PageableDefault(size = 5, page = 0, sort = "dateOfBirth", direction = Sort.Direction.DESC) Pageable pageable) {
+    return ResponseEntity.ok(naturalPersonService.getAllSortedByDateOfBirth(pageable));
   }
 }
